36af0a36c05a29ff36252cef5284cbbede1b8129
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2020-01-05 05:36:34 +0000

    Create yaml for the k8s's deployment

--- get_url_k8s.sh
+++ get_url_k8s.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-IP=$(minikube ip)
-NODE_PORT=$(kubectl get services dash-app -o go-template='{{(index .spec.ports 0).nodePort}}')
-echo "http://$IP:$NODE_PORT/dash"
+DEPLOY=dash-app
+echo $(minikube service $DEPLOY --url)
